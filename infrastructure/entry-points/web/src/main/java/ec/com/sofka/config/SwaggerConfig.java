a9c9050755a187b2ed8636afb57eb51e13dad6fb
Josefina-Mayans-Dev <maria.mayans@sofka.com.co>
2025-01-14 13:42:11 -0300

    fix:added security to swagger config

--- infrastructure/entry-points/web/src/main/java/ec/com/sofka/config/SwaggerConfig.java
+++ infrastructure/entry-points/web/src/main/java/ec/com/sofka/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package ec.com.sofka.config;
 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +17,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Banking System API")
                         .version("v1")
-                        .description("API Documentation for the Banking System"));
+                        .description("API Documentation for the Banking System"))
+                        .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                        .components(new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
-}
+    }
+
 
